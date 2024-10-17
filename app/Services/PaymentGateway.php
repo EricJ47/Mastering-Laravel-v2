6825bac33b1257a3be73e25be85bb328c69f8915
Eric Jamal <ericjamal06@gmail.com>
2024-10-17 13:07:17 +0700

    added services with dependency injection

---
+++ app/Services/PaymentGateway.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Services;
+
+class PaymentGateway
+{
+    public $apiKey ;
+    public function __construct(string $apiKey)
+    {
+        $this->apiKey = $apiKey;
+
+        
+    }
+    public function execute()
+    {
+        return 'payment Gateway';
+    }
+}
