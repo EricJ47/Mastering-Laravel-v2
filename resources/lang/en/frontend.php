1d691d1af73ee6b8cb33929c2f906e967830fc95
Eric Jamal <ericjamal06@gmail.com>
2024-10-23 14:14:47 +0700

    localization fixed

---
+++ resources/lang/en/frontend.php
@@ -0,0 +1,11 @@
+<?php 
+
+return [
+    'Wellcome HOme homie' => 'Wellcome HOme homie',
+    'Localization in Laravel to translation various languages'=> 'Localization in Laravel to translation various languages',
+    'Home'=> 'Home',
+    'About'=> 'About',
+    'Contact'=> 'Contact',
+    'More'=> 'More',
+    ''=> '',
+];
